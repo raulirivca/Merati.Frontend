b4b422d8d2c2da41e4d109557c94b74c1747478b
Raul Carlos Abelardo Rivera Castañeda <raul.rivera@gestionysistemas.com>
2023-11-20 01:50:57 -0500

    Integracion Login

---
+++ src/app/services/perfil.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PerfilService {
+  usuario: string='';
+  password: string='';
+
+  constructor(private httpclient: HttpClient) { }
+
+  autenticarUsuario(usuario:string,password:string): Observable<any> {
+    const params = new HttpParams()
+      .set('usuario', usuario)
+      .set('password', password);
+    return this.httpclient.get(`${environment.apiUrl}/Perfil/AutenticarUsuario`,{ params });
+    
+  }
+
+}
